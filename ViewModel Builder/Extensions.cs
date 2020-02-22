24d3baa6ae1f983ffb7aee0d40102f607dcb22ed
Ivan Stoychev <ivanstoychev44@gmail.com>
2020-02-22 11:45:18 +0200

    Initial commit.

---
+++ ViewModel Builder/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ViewModel_Builder
+{
+    static class Extensions
+    {
+        /// <summary>
+        /// Returns the input string with its first letter in upper case.
+        /// </summary>
+        /// <param name="str">The string whose first letter to set in upper case.</param>
+        /// <returns>The input string with its first letter in upper case.</returns>
+        public static string FirstLetterToUpper(this string str)
+        {
+            string firstLetterToUpper = str.First().ToString().ToUpper() + str.Substring(1);
+
+            return firstLetterToUpper;
+        }
+
+        /// <summary>
+        /// Returns the input string with its first letter in lower case.
+        /// </summary>
+        /// <param name="str">The string whose first letter to set in lower case.</param>
+        /// <returns>The input string with its first letter in lower case.</returns>
+        public static string FirstLetterToLower(this string str)
+        {
+            string firstLetterToLower = str.First().ToString().ToLower() + str.Substring(1);
+
+            return firstLetterToLower;
+        }
+    }
+}
---
+++ ViewModel Builder/MainWindowDataContext.cs
@@ -0,0 +1,120 @@
+﻿using Frontend;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ViewModel_Builder
+{
+    class MainWindowDataContext : INotifyPropertyChanged
+    {
+        public int PropertiesCount
+        {
+            get => PropertyViewsCollection.Count;
+            set
+            {
+                if (PropertyViewsCollection.Count != value)
+                {
+                    AdjustPropertiesCount(value);
+                }
+            }
+        }
+
+        public ObservableCollection<PropertyView> PropertyViewsCollection { get; }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        public RelayCommand AddPropertyCommand { get; }
+        public RelayCommand RemovePropertyCommand { get; }
+        public RelayCommand ExecuteCommand { get; }
+
+        public MainWindowDataContext()
+        {
+            PropertyViewsCollection = new ObservableCollection<PropertyView>();
+            AddPropertyCommand = new RelayCommand(OnAddProperty);
+            RemovePropertyCommand = new RelayCommand(OnRemoveProperty);
+            ExecuteCommand = new RelayCommand(OnExecute);
+        }
+
+        void OnAddProperty()
+        {
+            PropertyViewsCollection.Add(new PropertyView());
+            OnPropertyChanged("PropertiesCount");
+        }
+
+        void OnRemoveProperty()
+        {
+            PropertyViewsCollection.RemoveAt(PropertyViewsCollection.Count - 1);
+            OnPropertyChanged("PropertiesCount");
+        }
+
+        private void OnExecute()
+        {
+            string propertyTypeMergeField = "{propertyType}";
+            string privatePropertyNameMergeField = "{privatePropertyName}";
+            string publicPropertyNameMergeField = "{publicPropertyName}";
+            string privatePropertyTemplate = File.ReadAllText("Private property template.txt");
+            string publicPropertyTemplate = File.ReadAllText("Public property template.txt");
+            string propertyChangedTemplate = File.ReadAllText("Property changed template.txt");
+            StringBuilder privatePropertiesSB = new StringBuilder();
+            StringBuilder publicPropertiesSB = new StringBuilder();
+
+            foreach (PropertyView propertyView in PropertyViewsCollection)
+            {
+                string propertyType = propertyView.tboxPropertyType.Text;
+                string propertyName = propertyView.tboxPropertyName.Text;
+                privatePropertiesSB.AppendLine(privatePropertyTemplate.Replace(propertyTypeMergeField, propertyType).Replace(privatePropertyNameMergeField, propertyName.FirstLetterToLower()));
+                publicPropertiesSB.AppendLine(publicPropertyTemplate.Replace(propertyTypeMergeField, propertyType).Replace(publicPropertyNameMergeField, propertyName.FirstLetterToUpper()).Replace(privatePropertyNameMergeField, propertyName.FirstLetterToLower()) + Environment.NewLine);
+            }
+
+            privatePropertiesSB.AppendLine();
+            publicPropertiesSB.AppendLine(propertyChangedTemplate);
+
+            string result = privatePropertiesSB.ToString() + publicPropertiesSB.ToString();
+
+            ExportToNotepad(result);
+        }
+
+        void AdjustPropertiesCount(int count)
+        {
+            int difference = count - PropertyViewsCollection.Count;
+
+            if (difference > 0)
+            {
+                for (int i = 0; i < difference; i++)
+                {
+                    OnAddProperty();
+                }
+            }
+            else
+            {
+                difference = Math.Abs(difference);
+                for (int i = 0; i < difference; i++)
+                {
+                    OnRemoveProperty();
+                }
+            }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
+
+        void ExportToNotepad(string text)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("notepad");
+            startInfo.UseShellExecute = false;
+            Process notepad = Process.Start(startInfo);
+            notepad.WaitForInputIdle();
+            IntPtr child = FindWindowEx(notepad.MainWindowHandle, new IntPtr(0), null, null);
+            SendMessage(child, 0x000c, 0, text);
+        }
+    }
+}
