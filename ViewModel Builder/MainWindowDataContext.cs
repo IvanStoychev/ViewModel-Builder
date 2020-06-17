33c4794cc84a44dff2417d0902bb0ab7c52d7952
Ivan Stoychev <ivanstoychev44@gmail.com>
2020-06-17 03:09:43 +0300

    Removed unnecessary usings in solution.

--- ViewModel Builder/MainWindowDataContext.cs
+++ ViewModel Builder/MainWindowDataContext.cs
@@ -1,11 +1,9 @@
 ﻿using Frontend;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 
