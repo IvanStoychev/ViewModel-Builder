496467713a2f03b0995b9089c479c691b4419b65
Ivan Stoychev <ivanstoychev44@gmail.com>
2020-12-26 02:16:21 +0200

    - Updated projects to .NET 5. - Moved "Generate ViewModel" logic from "Frontend" to "Backend" project. - Finished "Generate ViewModel" functionality. - Changed "Backend" project to be of type "Class Library".
    - Some name changes and a usercontrol change.

---
+++ Backend/ViewModelBuilder.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using ViewModel_Builder.Utility;
+
+namespace Backend
+{
+    public static class ViewModelBuilder
+    {
+        public static void GenerateViewModelText(IConfigurationRoot configuration, List<(string Name, string Type)> propertyNameAndTypeDict)
+        {
+            string propertyTypeMergeField = configuration["PropertyTypeMergeField"];
+            string privatePropertyNameMergeField = configuration["PrivatePropertyNameMergeField"];
+            string publicPropertyNameMergeField = configuration["PublicPropertyNameMergeField"];
+            string privatePropertyTemplateFilepath = configuration["PrivatePropertyTemplate"];
+            string publicPropertyTemplateFilepath = configuration["PublicPropertyTemplate"];
+            string propertyChangedTemplateFilepath = configuration["PropertyChangedTemplate"];
+            string privatePropertyTemplate = File.ReadAllText(privatePropertyTemplateFilepath);
+            string publicPropertyTemplate = File.ReadAllText(publicPropertyTemplateFilepath);
+            string propertyChangedTemplate = File.ReadAllText(propertyChangedTemplateFilepath);
+            StringBuilder privatePropertiesSB = new StringBuilder();
+            StringBuilder publicPropertiesSB = new StringBuilder();
+
+            foreach (var NameAndType in propertyNameAndTypeDict)
+            {
+                string propertyType = NameAndType.Type;
+                string propertyName = NameAndType.Name;
+                string privatePropertyText = privatePropertyTemplate.Replace(propertyTypeMergeField, propertyType)
+                                                                    .Replace(privatePropertyNameMergeField, propertyName.FirstLetterToLower());
+
+                string publicPropertyText = publicPropertyTemplate.Replace(propertyTypeMergeField, propertyType)
+                                                                  .Replace(publicPropertyNameMergeField, propertyName.FirstLetterToUpper())
+                                                                  .Replace(privatePropertyNameMergeField, propertyName.FirstLetterToLower()) + Environment.NewLine;
+
+                privatePropertiesSB.AppendLine(privatePropertyText);
+                publicPropertiesSB.AppendLine(publicPropertyText);
+            }
+
+            privatePropertiesSB.AppendLine();
+            publicPropertiesSB.AppendLine(propertyChangedTemplate);
+
+            string result = privatePropertiesSB.ToString() + publicPropertiesSB.ToString();
+
+            ExportToNotepad(result);
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
+
+        /// <summary>
+        /// Opens notepad.exe and send the given text to it.
+        /// </summary>
+        /// <param name="text">The text to send to notepad.exe.</param>
+        static void ExportToNotepad(string text)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("notepad");
+            startInfo.UseShellExecute = false;
+            Process notepad = Process.Start(startInfo);
+            notepad.WaitForInputIdle();
+            IntPtr child = FindWindowEx(notepad.MainWindowHandle, new IntPtr(0), null, null);
+            SendMessage(child, 0x000c, 0, text);
+        }
+    }
+}
---
+++ Frontend/MainWindowDataContext.cs
@@ -0,0 +1,100 @@
+﻿using Frontend.Utility;
+using Frontend.Views;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using Backend;
+
+namespace Frontend
+{
+    class MainWindowDataContext : INotifyPropertyChanged
+    {
+        readonly IConfigurationRoot configuration;
+
+        /// <summary>
+        /// Amount of properties to generate.
+        /// </summary>
+        public int PropertiesCount
+        {
+            get => AddPropertyControlsCollection.Count;
+            set
+            {
+                if (AddPropertyControlsCollection.Count != value)
+                    PopulatePropertiesControls(value);
+            }
+        }
+
+        public List<string> PropertyTypes { get; }
+
+        /// <summary>
+        /// Holds all the PropertyViews on the MainWindow.
+        /// </summary>
+        public ObservableCollection<AddPropertyControl> AddPropertyControlsCollection { get; }  
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        public RelayCommand AddPropertyCommand { get; }
+        public RelayCommand RemovePropertyCommand { get; }
+        public RelayCommand ExecuteCommand { get; }
+
+        public MainWindowDataContext()
+        {
+            PropertyTypes = new List<string>() { "bool", "byte", "sbyte", "char", "decimal", "double", "float", "int", "uint", "long", "ulong", "short", "ushort" };
+            AddPropertyControlsCollection = new ObservableCollection<AddPropertyControl>();
+            AddPropertyCommand = new RelayCommand(OnAddProperty);
+            RemovePropertyCommand = new RelayCommand(OnRemoveProperty);
+            ExecuteCommand = new RelayCommand(OnExecute);
+
+            configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+        }
+
+        /// <summary>
+        /// Adds a new <see cref="AddPropertyControl"/> to the AddPropertyControlsCollection.
+        /// </summary>
+        void OnAddProperty()
+        {
+            AddPropertyControlsCollection.Add(new AddPropertyControl() { PropertyTypes = PropertyTypes });
+            OnPropertyChanged("PropertiesCount");
+        }
+
+        /// <summary>
+        /// Removes the last AddPropertyControl from the AddPropertyControlsCollection.
+        /// </summary>
+        void OnRemoveProperty()
+        {
+            AddPropertyControlsCollection.RemoveAt(AddPropertyControlsCollection.Count - 1);
+            OnPropertyChanged("PropertiesCount");
+        }
+
+        /// <summary>
+        /// Generates a set of properties and event handler for an MVVM viewmodel.
+        /// </summary>
+        private void OnExecute()
+        {
+            List<(string Name, string Type)> propertyNameAndTypeList = new();
+
+            foreach (AddPropertyControl AddPropertyControl in AddPropertyControlsCollection)
+                propertyNameAndTypeList.Add((AddPropertyControl.tboxPropertyName.Text, AddPropertyControl.cbPropertyType.Text));
+
+            ViewModelBuilder.GenerateViewModelText(configuration, propertyNameAndTypeList);
+        }
+
+        /// <summary>
+        /// Creates the given amount of PropertyViews for property generation.
+        /// </summary>
+        /// <param name="count">The total number of PropertyViews that should exist.</param>
+        void PopulatePropertiesControls(int count)
+        {
+            int difference = count - AddPropertyControlsCollection.Count;
+
+            if (difference > 0)
+                for (int i = 0; i < difference; i++)
+                    OnAddProperty();
+            else if (difference < 0)
+                for (int i = 0; i > difference; i--)
+                    OnRemoveProperty();
+        }
+    }
+}
