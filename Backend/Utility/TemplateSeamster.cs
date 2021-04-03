2ca9f95b5365b943f0dfb6741f7aeb565d8c840e
Ivan Stoychev <ivanstoychev44@gmail.com>
2021-04-03 20:15:27 +0300

    Added "TemplateSeamster" class that will handle preparation of templates.

---
+++ Backend/Utility/TemplateSeamster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Backend.Utility
+{
+    /// <summary>
+    /// Prepares templates for use.
+    /// </summary>
+    static class TemplateSeamster
+    {
+        /// <summary>
+        /// Replaces all occurrences in the given <paramref name="template"/> of all keys in the given <paramref name="mergeFieldsAndValues"/> dictionary with their values.
+        /// </summary>
+        /// <param name="template">A template for use in the building of a view model with merge fields.</param>
+        /// <param name="mergeFieldsAndValues">A dictionary with merge fields as keys and the values that should replace them as values.</param>
+        /// <returns></returns>
+        internal static string PrepareTemplate(string template, Dictionary<string, string> mergeFieldsAndValues)
+        {
+            foreach (var pair in mergeFieldsAndValues)
+                template = template.Replace(pair.Key, pair.Value);
+
+            return template;
+        }
+    }
+}
